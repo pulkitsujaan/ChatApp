85a026e733c5430ecbd46d38bb68c886539fb339
Pulkit Sujaan <pulkitsujaan2708@gmail.com>
2026-02-22 16:04:03 +0530

    Configured database and setup rooms

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const connect = require('./config/database-config');
 
 const app = express();
 const server = http.createServer(app);
@@ -8,20 +9,35 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
 
-  socket.on("from client", () => {
-    console.log("event coming from client");
-  });
+  socket.on('join_room',(data)=>{
+    console.log('joining a room', data);
+    socket.join(data.roomid);
+  })
+
+  // socket.on("from client", () => {
+  //   console.log("event coming from client");
+  // });
 
   socket.on('msg_send',(data)=>{
     console.log(data);
-    io.emit('msg_rcvd', data);
+    io.to(data.roomid).emit('msg_rcvd', data);
   })
 });
 
+
+app.set('view engine','ejs');
 app.use("/", express.static(__dirname + "/public"));
 
-server.listen(3000, () => {
+app.get('/chat/:roomid',(req,res)=>{
+  res.render('index',{
+    name:'Pulkit',
+    id: req.params.roomid
+  });
+})
+
+server.listen(3000, async () => {
   console.log("Server Started");
+  await connect();
+  console.log("mongodb connected");
 });
